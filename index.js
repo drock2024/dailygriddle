e51887c2093b6f40e3b4d725d71ed52bdfd94474
drock2024 <drock2024@gmail.com>
2026-01-06 12:20:39 -0500

    basic wordle functionality final

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 const BACKSPACE_KEY = 'Backspace';
 const ENTER_KEY = 'Enter';
+const WORD_LIST = [
+    'SHAKE', 'PASTA', 'PANIC',
+    'SKILL', 'ARROW', 'BIRDS'
+];
+const WORD_OF_THE_DAY = WORD_LIST[0];
 
 const MAX_NUMBER_OF_ATTEMPTS = 6;
 const history = [];
@@ -21,6 +26,9 @@ const init = () => {
 
     document.addEventListener('keydown', event => onKeyDown(event.key));
     keyboard.addEventListener('click', onKeyboardButtonClick);
+    gameBoard.addEventListener('animationend', event => {
+        event.target.setAttribute('data-animation', 'idle');
+    });
 }
 
 const generateBoard = (board, rows = 6, columns = 5, keys = [], keyboard = false) => {
@@ -90,6 +98,23 @@ const onKeyDown = (key) => {
         return;
     }
 
+    if (key === ENTER_KEY) {
+        console.log(currentWord.length);
+        if (currentWord.length < 5) {
+            showMessage('Too short');
+            return;
+        }
+
+        if (currentWord.length === 5 && WORD_LIST.includes(currentWord)) {
+            checkGuess(currentWord, WORD_OF_THE_DAY);
+        } else {
+            currentRow.setAttribute('data-animation', 'invalid');
+            showMessage('Not a valid word');
+        }
+
+        return;
+    }
+
     if (currentWord.length >= 5) return;
 
     const upperCaseLetter = key.toUpperCase();
@@ -101,25 +126,78 @@ const onKeyDown = (key) => {
         targetColumn.setAttribute('data-status', 'filled');
         targetColumn.setAttribute('data-animation', 'pop');
     }
+}
 
-    const showMessage = (message) => {
-        const toast = document.createElement('li');
+const showMessage = (message) => {
+    const toast = document.createElement('li');
 
-        toast.textContent = message;
-        toast.className = 'toast';
+    toast.textContent = message;
+    toast.className = 'toast';
 
-        document.querySelector('.toaster ul').prepend(toast);
-        setTimeout(() => toast.classList.add('fade'), 1000);
+    document.querySelector('.toaster ul').prepend(toast);
+    setTimeout(() => toast.classList.add('fade'), 1000);
 
-        toast.addEventListener('transitionend', (event) => event.target.remove());
-    }
+    toast.addEventListener('transitionend', (event) => event.target.remove());
+}
 
-    if (key === ENTER_KEY) {
-        if (currentWord.length < 5) {
-            showMessage('Too short');
-            return;
+const checkGuess = (guess, word) => {
+    const guessLetters = guess.split('');
+    const wordLetters = word.split('');
+    const remainingWordLetters = [];
+    const remainingGuessLetters = [];
+    //Find the active row
+    const currentRow = document.querySelector(`#board ul[data-row='${history.length}']`);
+
+    //Give all columns in current row base values
+    currentRow.querySelectorAll('li').forEach((element, index) => {
+        element.setAttribute('data-status', 'none');
+        element.setAttribute('data-animation', 'flip');
+
+        element.style.animationDelay = `${index * 300}ms`;
+        element.style.transitiondelay = `${index *400}ms`;
+    });
+
+    //Find all valid letters and create list of leftover letters
+    wordLetters.forEach((letter, index) => {
+        if (guessLetters[index] === letter) {
+            currentRow.querySelector(`li:nth-child(${index + 1})`).setAttribute('data-status', 'valid');
+            document.querySelector(`[data-key='${letter}']`).setAttribute('data-status', 'valid');
+
+            remainingWordLetters.push(false);
+            remainingGuessLetters.push(false);
+        } else {
+            remainingWordLetters.push(letter);
+            remainingGuessLetters.push(guessLetters[index]);
         }
-    }
+    });
+
+    //Find all misplaced letters
+    remainingWordLetters.forEach(letter => {
+        if (letter === false) return;
+
+        if (remainingGuessLetters.indexOf(letter) !== -1) {
+            const column = currentRow.querySelector(`li:nth-child(${remainingGuessLetters.indexOf(letter) + 1})`);
+            column.setAttribute('data-status', 'invalid');
+
+            const keyboardKey = document.querySelector(`[data-key='${letter}']`);
+
+            if (keyboardKey.getAttribute('data-status') !== 'valid') {
+                keyboardKey.setAttribute('data-status', 'invalid');
+            }
+        }
+    });
+
+    //Find all letters on the keyboard that are absent from the word
+    guessLetters.forEach(letter => {
+        const keyboardKey = document.querySelector(`[data-key='${letter}']`);
+
+        if (keyboardKey.getAttribute('data-status' === 'empty')) {
+            keyboardKey.setAttribute('data-status', 'none');
+        }
+    });
+
+    history.push(currentWord);
+    currentWord = '';
 }
 
 //Call the initilaization function when the DOM is loaded to get
